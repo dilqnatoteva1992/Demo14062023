300247a424b18dde9f5e6e419c27ee95a077cdb3
Dilyana Toteva <dtoteva@david.bg>
2023-06-14 16:19:41 +0300

    Add some calculations.

--- DemoSolution/DemoProject/Program.cs
+++ DemoSolution/DemoProject/Program.cs
@@ -9,6 +9,9 @@
 
             Console.WriteLine("Sqare area for square with side 2 is:");
             Console.Write(GetSqareArea(2));
+
+            Console.WriteLine("Sqare area for square with side 3 is:");
+            Console.Write(GetSqareArea(2));
         }
 
         public static double GetRectangleArea(double a, double b)
