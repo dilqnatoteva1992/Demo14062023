bb2695d76a11273a687f2e7c1685bc5c4b38f493
Dilyana Toteva <dtoteva@david.bg>
2023-06-14 16:11:15 +0300

    Add method for calculate square area.

--- DemoSolution/DemoProject/Program.cs
+++ DemoSolution/DemoProject/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DemoProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Rectangle area for rectangle with sides 2 and 3 is:");
+            Console.Write(GetRectangleArea(2,3));
+
+            Console.WriteLine("Sqare area for square with side 2 is:");
+            Console.Write(GetSqareArea(2));
+        }
+
+        public static double GetRectangleArea(double a, double b)
+        {
+            double area = a * b;
+
+            return area;
+        }
+
+        public static double GetSqareArea(double a)
+        {
+            double area = a * a;
+
+            return area;
+        }
+    }
+}
